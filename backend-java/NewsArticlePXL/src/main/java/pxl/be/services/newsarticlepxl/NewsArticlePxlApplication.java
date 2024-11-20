32af43970a8f15a5e951202266299693467ae5f1
nicky.schobben@student.pxl.be <nicky.schobben@student.pxl.be>
2024-11-20 09:53:22 +0100

    Starting base for full stack java project. Ran all three services and they work.

---
+++ backend-java/NewsArticlePXL/src/main/java/pxl/be/services/newsarticlepxl/NewsArticlePxlApplication.java
@@ -0,0 +1,13 @@
+package pxl.be.services.newsarticlepxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsArticlePxlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsArticlePxlApplication.class, args);
+    }
+
+}
---
+++ backend-java/NewsArticlePXL/src/test/java/pxl/be/services/newsarticlepxl/NewsArticlePxlApplicationTests.java
@@ -0,0 +1,13 @@
+package pxl.be.services.newsarticlepxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsArticlePxlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
